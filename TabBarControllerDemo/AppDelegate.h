ba4d8dba08e16c1a093471a4858bc6da4bf88f4d
neelasantosh <neela.santosh2405@gmail.com>
2016-07-31 16:30:54 +0530

    tabbar

---
+++ TabBarControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TabBarControllerDemo
+//
+//  Created by Rajesh on 19/12/15.
+//  Copyright © 2015 CDAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TabBarControllerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TabBarControllerDemo
+//
+//  Created by Rajesh on 19/12/15.
+//  Copyright © 2015 CDAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
